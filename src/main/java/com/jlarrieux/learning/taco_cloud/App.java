6c264133ec5efd60984b5511e9378c3a21e99d8b
mrseanpaul81 <jeanrodneylarrieux@gmail.com>
2017-07-27 16:05:45 -0500

    first commit

---
+++ src/main/java/com/jlarrieux/learning/taco_cloud/App.java
@@ -0,0 +1,18 @@
+package com.jlarrieux.learning.taco_cloud;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args){
+        SpringApplication.run(App.class, args);
+    }
+
+
+}
