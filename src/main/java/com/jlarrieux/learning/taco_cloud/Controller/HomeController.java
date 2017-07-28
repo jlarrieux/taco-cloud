c952d8fc58794c2189307ad61c7d30e18d5696c4
Jeannius <jeanrodneylarrieux@gmail.com>
2017-07-27 21:30:11 -0500

    simple html page working

---
+++ src/main/java/com/jlarrieux/learning/taco_cloud/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.jlarrieux.learning.taco_cloud.Controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+}
---
+++ src/test/java/com/jlarrieux/learning/taco_cloud/test/AppTests.java
@@ -0,0 +1,22 @@
+package com.jlarrieux.learning.taco_cloud.test;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTests {
+
+
+    @Test
+    public void contextLoads(){
+
+    }
+
+}
